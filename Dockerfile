90dc655961c4a117752a45ee87b3993d152be80a
Tony Tran <tonytran20081997@gmail.com>
2024-09-06 17:49:51 +0700

    - up

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM mongo:6.0
+FROM mongo:8.0.0-rc19-noble
 
+# Tạo thư mục cho các script khởi tạo
 RUN mkdir -p /docker-entrypoint-initdb.d
 
+# Sao chép file khởi tạo vào thư mục khởi tạo
 COPY init/init-db.js /docker-entrypoint-initdb.d/
 
+# Khởi động MongoDB với xác thực bật
 CMD ["mongod", "--auth"]
--- init/init-db.js
+++ init/init-db.js
@@ -1,14 +1,23 @@
+// Lấy giá trị từ biến môi trường (được inject bởi Kubernetes Secrets)
+var adminUser = process.env.MONGO_INITDB_ROOT_USERNAME;
+var adminPass = process.env.MONGO_INITDB_ROOT_PASSWORD;
+
+// Chuyển sang database 'admin'
 db = db.getSiblingDB('admin');
 
+// Tạo user admin
 db.createUser({
-  user: process.env.MONGO_INITDB_ROOT_USERNAME,
-  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+  user: adminUser,
+  pwd: adminPass,
   roles: [ { role: "root", db: "admin" } ]
 });
 
+// Chuyển sang database 'mydatabase'
 db = db.getSiblingDB('mydatabase');
 
+// Tạo collection và thêm dữ liệu mặc định vào 'mycollection'
 db.mycollection.insertMany([
   { name: "John Doe", email: "john@example.com" },
-  { name: "Jane Smith", email: "jane@example.com" }
+  { name: "Jane Smith", email: "jane@example.com" },
+  { name: "Tony Smith", email: "tony@example.com" }
 ]);
