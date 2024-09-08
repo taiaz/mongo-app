47e1fbabf50ed1bfc69543c52425fd555337bfb0
Tony Tran <tonytran20081997@gmail.com>
2024-09-08 10:22:00 +0700

    - up

--- init/init-db.js
+++ init/init-db.js
@@ -1,7 +1,8 @@
+
 var adminUser = process.env.MONGO_INITDB_ROOT_USERNAME;
 var adminPass = process.env.MONGO_INITDB_ROOT_PASSWORD;
 
-db = db.getSiblingDB('user_management');
+db = db.getSiblingDB('admin');
 
 db.createUser({
   user: adminUser,
@@ -11,10 +12,8 @@ db.createUser({
 
 db = db.getSiblingDB('user_management');
 
-db.createCollection("users");
 db.users.insertMany([
   { name: "John Doe", email: "john@example.com" },
   { name: "Jane Smith", email: "jane@example.com" },
   { name: "Tony Smith", email: "tony@example.com" }
 ]);
-
