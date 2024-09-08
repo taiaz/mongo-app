d6d87d3c55a871fe813d8bdda3083abf4d6546e1
Tony Tran <tonytran20081997@gmail.com>
2024-09-08 09:52:44 +0700

    - up

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,7 @@
 FROM mongo:8.0.0-rc19-noble
 
-# Tạo thư mục cho các script khởi tạo
 RUN mkdir -p /docker-entrypoint-initdb.d
 
-# Sao chép file khởi tạo vào thư mục khởi tạo
 COPY init/init-db.js /docker-entrypoint-initdb.d/
 
-# Khởi động MongoDB với xác thực bật
 CMD ["mongod", "--auth"]
--- init/init-db.js
+++ init/init-db.js
@@ -1,21 +1,16 @@
-// Lấy giá trị từ biến môi trường (được inject bởi Kubernetes Secrets)
 var adminUser = process.env.MONGO_INITDB_ROOT_USERNAME;
 var adminPass = process.env.MONGO_INITDB_ROOT_PASSWORD;
 
-// Chuyển sang database 'user_management'
 db = db.getSiblingDB('user_management');
 
-// Tạo user admin với quyền root
 db.createUser({
   user: adminUser,
   pwd: adminPass,
   roles: [ { role: "root", db: "admin" } ]
 });
 
-// Chuyển sang database 'user_management'
 db = db.getSiblingDB('user_management');
 
-// Tạo collection 'users' và thêm dữ liệu mặc định
 db.createCollection("users");
 db.users.insertMany([
   { name: "John Doe", email: "john@example.com" },
@@ -23,5 +18,4 @@ db.users.insertMany([
   { name: "Tony Smith", email: "tony@example.com" }
 ]);
 
-// Cấp quyền readWrite cho user 'admin' trên 'user_management'
 db.getSiblingDB('admin').grantRolesToUser(adminUser, [{ role: "readWrite", db: "user_management" }]);
