db7365cdbd1038e0b2d0d160427dc7d59be9e39d
Tony Tran <tonytran20081997@gmail.com>
2024-09-10 00:52:43 +0700

    - up

--- init/init-db.js
+++ init/init-db.js
@@ -1,23 +1,31 @@
 var adminUser = "ADMIN_USER_PLACEHOLDER";
 var adminPass = "ADMIN_PASS_PLACEHOLDER";
 
+// Kết nối tới database admin
 db = db.getSiblingDB('admin');
 
 // Kiểm tra xem người dùng có tồn tại không
 if (db.getUser(adminUser) === null) {
+  // Tạo người dùng admin với quyền truy cập trên cả admin và user_management
   db.createUser({
     user: adminUser,
     pwd: adminPass,
-    roles: [{ role: "root", db: "admin" }]
+    roles: [
+      { role: "root", db: "admin" },  // Quyền root trên admin
+      { role: "dbOwner", db: "user_management" }  // Quyền dbOwner trên user_management
+    ]
   });
 }
 
+// Kết nối tới database user_management
 db = db.getSiblingDB('user_management');
 
 // Kiểm tra xem collection users có tồn tại chưa
 if (!db.getCollectionNames().includes('users')) {
+  // Tạo collection nếu chưa tồn tại
   db.createCollection("users");
 
+  // Thêm dữ liệu vào collection users
   db.users.insertMany([
     { name: "John Doe", email: "john@example.com" },
     { name: "Jane Smith", email: "jane@example.com" },
